c8d5eb31b848bcca7be2207726d7f9269fb9635a
ethiodsf <ethiodsf@yahoo.com>
2020-10-11 10:49:16 -0400

    recent form update

---
+++ site/bootstrap/js/memberShipForm.js
@@ -0,0 +1,4 @@
+// JavaScript source code
+function print_current_page() {
+    window.print();
+}
--- site/mainPage.js
+++
@@ -1,4 +0,0 @@
-// JavaScript source code
-document.body.style.background = "lightgreen";
-// change it back after 1 second
-setTimeout(() => document.body.style.background = "", 3000);
